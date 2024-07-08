c5c7502b1790782a952028bddc3a970acde65158
Juyeob Lee <lcg0116@gmail.com>
2024-07-09 00:16:11 +0900

    2024-07-08 simply complete

--- main.js
+++ main.js
@@ -8,10 +8,16 @@
 
 let taskInput = document.getElementById("task-input");
 let addButton = document.getElementById("button-add");
+let tabs = document.querySelectorAll(".tab-type div")
 let taskList = [];
+let filterList = [];
+let mode = 'all'
+
 addButton.addEventListener("click", addTask);
 
-
+for(let i=1; i<tabs.length; i++){
+    tabs[i].addEventListener("click", function(event){filter(event)})
+}
 
 
 function addTask(){
@@ -22,7 +28,6 @@ function addTask(){
     }
     taskList.push(task);
     render()
-    console.log(taskList)
 }
 
 function togglecomplete(id){
@@ -42,25 +47,64 @@ function deleteTask(id){
             break
         }
     }
-    render()
+    filter(event)
+}
+
+function filter(event){
+    filterList = []
+    mode = event.target.id
+    if(mode == "all"){
+        // 전체 리스트를 보여준다.
+        render()
+    }
+    else if(mode == "ongoing"){
+        // 진행중인 아이템을 보여준다.
+        // task.isComplet=false
+        for(let i = 0; i < taskList.length; i++){
+            if(taskList[i].isComplete == false){
+                filterList.push(taskList[i])
+            }
+        }
+        render()
+    }
+    else if(mode == "done"){
+        // 끝나는 케이스
+        // task.isComplet=true
+        for(let i = 0; i < taskList.length; i++){
+            if(taskList[i].isComplete == true){
+                filterList.push(taskList[i])
+            }
+        }
+        render()
+    }
 }
 function render(){
+    let list=[]
+    // 1. 내가 선택한 탭에 따라서
+    if(mode == "all"){
+        list = taskList;
+    }else if(mode == "ongoing" || mode == "done"){
+        list = filterList;
+    }
+    // 2. 리스트를 달리 보여준다.
+    // all은 taskList
+    // ongoing, done은 filterList
     let resultHTML = "";
-    for(let i=0; i < taskList.length; i++){
-        if(taskList[i].isComplete == true){
+    for(let i=0; i < list.length; i++){
+        if(list[i].isComplete == true){
             resultHTML += `<div class="task">
-            <div class = "task-done">${taskList[i].taskContent}</div>
+            <div class = "task-done">${list[i].taskContent}</div>
                 <div class = "button">
-                    <button onclick="togglecomplete('${taskList[i].id}')" type="button">check</button>
-                    <button onclick="deleteTask('${taskList[i].id}')">Delete</button>
+                    <button onclick="togglecomplete('${list[i].id}')" type="button">check</button>
+                    <button onclick="deleteTask('${list[i].id}')">Delete</button>
                 </div>
             </div>`
         }else{
             resultHTML += `<div class="task">
-            <div>${taskList[i].taskContent}</div>
+            <div>${list[i].taskContent}</div>
                 <div class = "button">
-                    <button onclick="togglecomplete('${taskList[i].id}')" type="button">check</button>
-                    <button onclick="deleteTask('${taskList[i].id}')">Delete</button>
+                    <button onclick="togglecomplete('${list[i].id}')" type="button">check</button>
+                    <button onclick="deleteTask('${list[i].id}')">Delete</button>
                 </div>
             </div>`
         }
